f0a7af6453a708464b31804841cf45ee22dc2aa7
Mihail <128997830+beliy122121@users.noreply.github.com>
2023-04-08 16:04:22 +0300

    Add files via upload

---
+++ проект 1.js
@@ -0,0 +1,73 @@
+// 1 Переменнные
+//camelCase
+const firstName='Mihail'
+const LastName='Krestovcev'
+//const isProgramer=true // boolean
+
+//const _='private'
+//const $='some value'
+
+//const witeNum5='5'
+
+// 2 Мутирование
+//console.log('Имя человека'+firstName)
+//prompt('введите фамилию')
+// 3 Операторы
+//let currentYear = 2023
+//const birthYear=1989
+//const age=currentYear-birthYear
+//const a=10
+// b=5
+//let c=32
+//console.log(a+b)
+//console.log (a-b)
+//console.log(a*b)
+//console.log(a/b)
+//console.log(++currentYear)
+//console.log(--currentYear)
+
+// 4 Типы данных
+//const isProgrammer=true
+//const name='Mihail'
+//const age =33
+//let x
+//console.log(typeof isProgrammer)
+//console.log(typeof name)
+//console.log(typeof age)
+//console.log(typeof x)
+
+// 5 Приоритет операции
+//const FullAge = 33
+//const birthYear = 1989
+//const currentYear=2023
+//const isFullAge=currentYear-birthYear >= FullAge
+//console.log (isFullAge)
+
+// 6 Условные операторы
+//const courseStatus='pending'//ready,fail,pending
+//if(courseStatus==='ready'){
+    //console.log('Курс уже готов и его можно проходить')
+//}
+//else if(courseStatus==='pending'){
+    //console.log('Курс пока находится в поцессе разработки')
+//}else{
+    //console.log('Курс не получился')
+//}
+//const isReady=true
+//if (isReady){
+   // console.log('Все готово')
+//}else{
+    //console.log('Все не готово')
+//}
+//const num1 = 42
+//const num2= '42'
+//console.log(num1===num2)
+
+// 8 Функции
+function calculateAge(year) {
+    return 2023 - year
+}
+const myAge =calculateAge(1989)
+console.log(myAge)
+
+
